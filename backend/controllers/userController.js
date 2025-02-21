d9b4b0a7987e5aa46655e2c2d80536360317003e
hardikWottaweb <hardik.wottaweb@gmail.com>
2025-02-21 11:27:35 +0530

    Change Password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const Users = require('../models/usersModel')
+const bcrypt = require('bcryptjs')
 
 // Get All Users
 const GetAllUsers = asyncHandler(async (req, res) => {
@@ -79,9 +80,41 @@ const EditUserProfile = asyncHandler(async(req, res) => {
     });
 })
 
+// CHANGE PASSWORD
+
+const ChangePassword = asyncHandler(async (req, res) => {
+    const { CurrPassword, NewPassword } = req.body
+    const userId = req.params.id
+    const user = await Users.findById(userId)
+
+    if(!user){
+        res.status(400).json({
+            message: "User Not Found"
+        })
+    }
+
+    const isMatch = await bcrypt.compare(CurrPassword, user.password);
+
+    if (!isMatch) {
+        return res.status(400).json({ message: "Incorrect Current Password" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(NewPassword, salt);
+
+    await user.save()
+
+    res.status(201).json({
+        message: 'Password Updated Successfully',
+    })
+
+})
+
+
 module.exports = {
     GetAllUsers,
     DeleteUserById,
     GetUserProfile,
-    EditUserProfile
+    EditUserProfile,
+    ChangePassword
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const { protect } = require('../middleware/authMiddleware');
-const { GetAllUsers, DeleteUserById, GetUserProfile, EditUserProfile } = require('../controllers/userController')
+const { GetAllUsers, DeleteUserById, GetUserProfile, EditUserProfile, ChangePassword } = require('../controllers/userController')
 
 router.route('/list').get(protect, GetAllUsers)
 router.route('/:id').delete(protect, DeleteUserById)
 router.route('/:id').get(protect, GetUserProfile)
 router.route('/:id').put(protect, EditUserProfile)
+router.route('/change-password/:id').put(protect, ChangePassword)
 
 module.exports = router
