ca6a6e85a188b839034c856cb686575ea9196e0f
hardikWottaweb <hardik.wottaweb@gmail.com>
2025-02-24 13:07:43 +0530

    Add New Address

--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -1,15 +1,19 @@
 const roleMiddleware = (requiredRoles) => {
     return (req, res, next) => {
-        if(!req.user){
-            res.status(401).json({ message: "Unauthorized"})
+        if (!req.user) {
+            return res.status(401).json({ message: "Unauthorized" });
         }
 
-        if(!requiredRoles.includes(req.user.role)){
-            return res.status(403).json({message: "Forbidden, You don't have permission"})
+        if (!req.user.role) {
+            return res.status(403).json({ message: "Forbidden: Role not found" });
+        }
+
+        if (!requiredRoles.includes(req.user.role)) {
+            return res.status(403).json({ message: "Forbidden: You don't have permission" });
         }
 
         next();
-    }
-}
+    };
+};
 
-module.exports = roleMiddleware
+module.exports = roleMiddleware;
