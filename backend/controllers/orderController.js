70a8ff3bf1baf5e429b84ede96fa9d82336a8ce2
hardikWottaweb <hardik.wottaweb@gmail.com>
2025-02-25 15:46:12 +0530

    Order place email template

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,8 +2,9 @@ const asyncHandler = require('express-async-handler')
 const Cart = require('../models/cartModel')
 const { User } = require('../models/usersModel')
 const Order = require('../models/orderModel')
-const { ClearCart } = require('./cartController')
 const calculateCartTotal = require('../utils/calcCartTotal')
+const Product = require('../models/productModel')
+const sendEmail = require('../utils/sendEmail')
 
 // Get Checkout Data
 
@@ -17,12 +18,14 @@ const placeOrder = asyncHandler(async(req,res) => {
         throw new Error('User not found')
     }
 
-
     const cart = await Cart.findOne({ userId })
-    if(!cart){
+    if(!cart || cart.items.length === 0){
         res.status(400)
         throw new Error('Cart is empty')
     }
+
+    const productId = cart.items.map(item => item.productId)
+    const products = await Product.find({ _id: { $in: productId } });
 
     const address = user.addresses.id(addressId);
     if(!address){
@@ -45,7 +48,35 @@ const placeOrder = asyncHandler(async(req,res) => {
 
    cart.items = []
    await cart.save()
-   
+
+    const productDetails = products.map(product => 
+        `<div style="border: 1px solid #fff; border-radius: 8px;">
+            <p><strong>Product:</strong> ${product.name}</p>
+            <p><strong>Price:</strong> $${product.price}</p>
+        </div>`
+        ).join('');
+
+    await sendEmail({
+        to: user.email,
+        subject: 'ORDER PLACED - SHOPPER',
+        text: `Your order has been placed successfully.`,
+        html: `
+            <div>
+                <h1>Thank you for Shopping with us!</h1>
+                <p>Thank you for ordering from <b>SHOPPERS</b>.
+                Your order will be delivered within 5 to 7 days to your address.</p>
+                <div>
+                    <h4>Address :</h4>
+                    <p>${order.shippingInfo.street}, ${order.shippingInfo.city}, 
+                    ${order.shippingInfo.state}, ${order.shippingInfo.country}, ${order.shippingInfo.zipCode}</p>
+                </div>
+                <h5>Your Orders</h5>
+                    <div>${productDetails}</div>
+                    <p style="text-align:end">Total Amount: $${totalAmount}</p>
+            </div>
+        `
+    });
+
    res.status(201).json({
         message: "Order placed Successfully",
         order
