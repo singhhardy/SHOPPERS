b1abe64e15aac73aa7b8d0504ff52a29ad3da4aa
hardikWottaweb <hardik.wottaweb@gmail.com>
2025-03-07 17:24:40 +0530

    minor changes profile

--- frontend/src/app/features/user/edit-user/edit-user.component.ts
+++ frontend/src/app/features/user/edit-user/edit-user.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angula
 import { CommonModule } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
 import { UserService } from '../../../core/services/user.service';
+import { AuthService } from '../../../core/services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-edit-user',
@@ -13,14 +15,16 @@ import { UserService } from '../../../core/services/user.service';
 })
 export class EditUserComponent {
   profileForm: FormGroup
+  user$: Observable<any>
 
-  constructor(private userAuth: UserService, private fb: FormBuilder, private toastr: ToastrService){
+  constructor(private userAuth: UserService, private fb: FormBuilder, private toastr: ToastrService, private auth: AuthService){
     this.profileForm = this.fb.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
       phone: ['', [Validators.required]],
-      dob: ['', [Validators.required]]
+      dateOfBirth: ['', [Validators.required]]
     })
+    this.user$ = this.auth.user$
   }
 
   onSubmit(){
