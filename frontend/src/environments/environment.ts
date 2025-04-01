d88f479e6dcfb85bc3c557545fca0e84923c42c7
hardikWottaweb <hardik.wottaweb@gmail.com>
2025-04-01 12:46:34 +0530

    UI fixes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    // baseUrl: 'https://shoppers-eight.vercel.app/api',
-    baseUrl: 'http://localhost:8000/api',
+    baseUrl: 'https://shoppers-eight.vercel.app/api',
+    // baseUrl: 'http://localhost:8000/api',
     development: true
 }
