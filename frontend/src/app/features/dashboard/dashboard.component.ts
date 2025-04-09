448054382d36ae5b40981bc03735301c9dff0bd2
hardikWottaweb <hardik.wottaweb@gmail.com>
2025-04-09 11:43:35 +0530

    home UI

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ProductListComponent } from "../products/product-list/product-list.component";
 import { CommonModule } from '@angular/common';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ProductListComponent, CommonModule, NgbCarouselModule ],
+  imports: [CommonModule, NgbCarouselModule, RouterModule ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
